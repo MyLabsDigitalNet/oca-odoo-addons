ddde32890123948456dcce79f13b6dd50ac85282
AdrianBernal <a2b1987@gmail.com>
2023-12-28 15:03:46 +0100

    Update all submodules

--- git_update_submodules.sh
+++ git_update_submodules.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # This script will navigate to every directory in the current folder
-# and execute "git checkout 15.0" and "git pull" commands.
+# and execute "git checkout 16.0" and "git pull" commands.
+
+# For use only in main repository not in submodules.
 
 for dir in */ ; do
   cd "$dir"
   echo "Entering directory: $(pwd)"
-  git checkout 15.0
+  git checkout 16.0
   git pull
   cd ..
 done
