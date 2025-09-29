55ff359f40665c4663f838fbf534799bb3cd85a7
AdrianBernal <a2b1987@gmail.com>
2025-09-29 17:11:28 +0200

    [ADD] add formio to remove them

---
+++ third_party/formio_mail/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+{
+    'name': 'Forms | Mail',
+    'summary': 'Send Forms via mail',
+    'version': '16.0.0.1',
+    'license': 'LGPL-3',
+    'author': 'Nova Code',
+    'website': 'https://www.novacode.nl',
+    # 'live_test_url': 'https://demo16.novacode.nl',
+    'category': 'Forms/Forms',
+    'depends': [
+        'mail',
+        'formio_components_synchronizer',
+        'formio_report_qweb'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/formio_builder_views.xml',
+        'views/formio_mail_recipient_address_views.xml',
+        'views/formio_menu.xml',
+    ],
+    'application': True,
+    'installable': False,
+    'images': [
+        'static/description/banner.gif',
+    ],
+    'description': "Send forms via mail after completion."
+}
